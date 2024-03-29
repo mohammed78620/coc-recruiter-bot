e2615855873ae3af2e28ac7a08d6d1e67a096825
Mohammed Akmal Miah <mohammedmiah67890@gmail.com>
2024-03-29 15:24:17 +0000

    direct message users functionality

---
+++ coc_recruiter_bot/funcs/send_message.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def send_message(token, channel_id, message):
+    url = "https://discord.com/api/v8/channels/{}/messages".format(channel_id)
+    data = {"content": message}
+    header = {"authorization": token}
+
+    r = requests.post(url, data=data, headers=header, timeout=1)
+    print(r.status_code)
+
+
+def create_dm_channel(token, user_id):
+    data = {"recipient_id": user_id}
+    headers = {"authorization": token}
+
+    r = requests.post("https://discord.com/api/v9/users/@me/channels", json=data, headers=headers, timeout=1)
+    print(r.status_code)
+
+    channel_id = r.json()["id"]
+
+    return channel_id
